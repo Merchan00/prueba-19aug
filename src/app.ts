c4ba37e2e0674942084a72b5944ea12de447eb8c
Julian Garcia <jcgrrincon@gmail.com>
2022-08-19 15:28:12 -0500

    servidor levantado

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Express} from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+
+abstract class ConfigServer {
+  protected app:Express = express();
+  constructor() {
+    this.app.set('PORT', 3000);
+    this.app.use(morgan('dev'));
+    this.app.use(cors());
+  }
+}
+
+export default ConfigServer;
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import ConfigServer from "./app";
+
+class Server extends ConfigServer {
+  private PORT:number;
+  constructor() {
+    super();
+    this.PORT = this.app.get('PORT');
+  }
+
+  public listen(){
+    this.app.listen(this.PORT, ()=>{
+      console.log("Server to Listen...")
+    })
+  }
+}
+
+const listenServer = new Server();
+
+listenServer.listen();
